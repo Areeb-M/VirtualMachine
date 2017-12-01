e5486b2fe3adc39e81ab65f876fda1db28fa0375
Areeb <Areeb@DESKTOP-MMMTE27>
2017-11-30 18:26:25 -0600

    Created list of native system types and their byte sizes

---
+++ core/sys/native_types.h
@@ -0,0 +1,16 @@
+
+#ifndef CORE_SYS_NATIVE_TYPES_H_
+#define CORE_SYS_NATIVE_TYPES_H_
+
+#include <stddef.h>
+
+const size_t HALF_WORD = 4;
+const size_t WORD = 8;
+const size_t DOUBLE_WORD = 16;
+
+const size_t BYTE = 1;
+const size_t FLOAT = 4;
+const size_t DOUBLE = 8;
+
+
+#endif /* CORE_SYS_NATIVE_TYPES_H_ */
